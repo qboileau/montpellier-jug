9dac23d3d46adf4983e6f895a32fc2aaaa58ed62
Romain Maneschi <romain@maneschi.fr>
2015-03-07 00:16:27 +0100

    add abstact form to edit any class/object easily

--- montpellier-jug-wisdom/src/main/java/sample/WelcomeController.java
+++ montpellier-jug-wisdom/src/main/java/sample/WelcomeController.java
@@ -19,18 +19,21 @@
  */
 package sample;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import model.News;
 
+import org.apache.felix.ipojo.annotations.Requires;
 import org.wisdom.api.DefaultController;
-import org.wisdom.api.annotations.Controller;
-import org.wisdom.api.annotations.Route;
-import org.wisdom.api.annotations.View;
+import org.wisdom.api.annotations.*;
 import org.wisdom.api.http.HttpMethod;
 import org.wisdom.api.http.Result;
 import org.wisdom.api.templates.Template;
+import services.PropertiesForm;
 
 /**
  * Your first Wisdom Controller.
@@ -61,17 +64,51 @@ public class WelcomeController extends DefaultController {
     
     @View("news")
     Template news;
-    
+
     List<News> news() {
         return Arrays.asList(
-            new News("Nouveau site", "Trop bien ;)"),
-            new News()
+            new News("Nouveau site", "Trop bien ;)")
         );
     }
     
     @Route(method = HttpMethod.GET, uri = "/news")
     public Result defaultView() {
         return ok(render(news, "news", news()));
+    }
+
+    /**
+     * FORM NEWS PART
+     */
+
+    @View("layouts/form")
+    Template formTemplate;
+
+    @Requires
+    PropertiesForm propertiesForm;
+
+    @Route(method = HttpMethod.GET, uri = "/addnews")
+    public Result addNewsGet() {
+        return ok(render(formTemplate, "properties", propertiesForm.get(News.class), "urlSubmit", "/addnews"));
+    }
+
+    @Route(method = HttpMethod.POST, uri = "/addnews")
+    public Result addNewsPost(@Body News myNews) {
+        List<News> allNews = new ArrayList<News>(news());
+        allNews.add(myNews);
+        return ok(render(news, "news", allNews));
+    }
+
+    @Route(method = HttpMethod.GET, uri = "/modnews/{id}")
+    public Result modNewsGet(@Parameter("id") int id) {
+        return ok(render(formTemplate, "properties", propertiesForm.get(news().get(id)), "urlSubmit", "/modnews/" + id));
+    }
+
+    @Route(method = HttpMethod.POST, uri = "/modnews/{id}")
+    public Result modNewsPost(@Body News myNews, @Parameter("id") int id) {
+        List<News> allNews = new ArrayList<News>(news());
+        allNews.remove(id);
+        allNews.add(myNews);
+        return ok(render(news, "news", allNews));
     }
 
 }
---
+++ montpellier-jug-wisdom/src/main/java/services/PropertyFrom.java
@@ -0,0 +1,12 @@
+package services;
+
+/**
+ * Created by manland on 06/03/15.
+ */
+public class PropertyFrom {
+
+    public String name;
+    public String type;
+    public String value;
+
+}
