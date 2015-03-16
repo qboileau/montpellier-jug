7457b7a7fa79b1dd0555732d0b25a2000cf5666a
Romain Maneschi <romain@maneschi.fr>
2015-03-17 00:37:52 +0100

    add year partners on all pages

--- montpellier-jug-wisdom/src/main/java/org/jug/montpellier/core/controller/JugController.java
+++ montpellier-jug-wisdom/src/main/java/org/jug/montpellier/core/controller/JugController.java
@@ -2,6 +2,7 @@ package org.jug.montpellier.core.controller;
 
 import org.jug.montpellier.core.api.CartridgeSupport;
 import org.jug.montpellier.core.api.NextEventSupport;
+import org.jug.montpellier.core.api.PartnerSupport;
 import org.wisdom.api.DefaultController;
 import org.wisdom.api.http.Result;
 import org.wisdom.api.templates.Template;
@@ -13,6 +14,7 @@ public class JugController extends DefaultController {
 
     public static final String CARTRIDGES = "cartridges";
     public static final String NEXT_EVENT = "nextEvent";
+    public static final String PARTNERS = "partners";
 
     /**
      * Use this contructor to display a normal page with full menu
@@ -21,6 +23,7 @@ public class JugController extends DefaultController {
     public JugController(CartridgeSupport cartridgeSupport) {
         this.cartridgeSupport = cartridgeSupport;
         this.nextEventSupport = null;
+        this.partnerSupport = null;
     }
 
     /**
@@ -28,13 +31,15 @@ public class JugController extends DefaultController {
      * @param cartridgeSupport all pages to display in menu
      * @param nextEventSupport the next event to display
      */
-    public JugController(CartridgeSupport cartridgeSupport, NextEventSupport nextEventSupport) {
+    public JugController(CartridgeSupport cartridgeSupport, NextEventSupport nextEventSupport, PartnerSupport partnerSupport) {
         this.cartridgeSupport = cartridgeSupport;
         this.nextEventSupport = nextEventSupport;
+        this.partnerSupport = partnerSupport;
     }
 
     private final CartridgeSupport cartridgeSupport;
     private final NextEventSupport nextEventSupport;
+    private final PartnerSupport partnerSupport;
 
     public Result renderRoot(Template template, Object...params) {
 
@@ -47,6 +52,10 @@ public class JugController extends DefaultController {
 
         if(nextEventSupport != null) {
             paramsMap.put(NEXT_EVENT, nextEventSupport.getNextEvent());
+        }
+
+        if(partnerSupport != null) {
+            paramsMap.put(PARTNERS, partnerSupport.getPartners());
         }
 
         return ok(render(template, paramsMap));
