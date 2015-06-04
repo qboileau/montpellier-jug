c3bc479d5e3cce7895b70b2be1368a9dee5ebfb8
Romain Maneschi <romain@maneschi.fr>
2015-06-04 21:54:40 +0200

    finish partner admin

--- montpellier-jug-wisdom/src/main/java/org/jug/montpellier/admin/controllers/AdminPartnerController.java
+++ montpellier-jug-wisdom/src/main/java/org/jug/montpellier/admin/controllers/AdminPartnerController.java
@@ -19,11 +19,19 @@
  */
 package org.jug.montpellier.admin.controllers;
 
+import com.google.common.collect.Maps;
 import org.apache.felix.ipojo.annotations.Requires;
+import org.jooq.DSLContext;
+import org.jooq.SelectOrderByStep;
+import org.jooq.SelectWhereStep;
 import org.jug.montpellier.core.api.JugSupport;
 import org.jug.montpellier.core.controller.JugController;
+import org.jug.montpellier.forms.apis.ListView;
 import org.jug.montpellier.forms.apis.PropertySheet;
+import org.jug.montpellier.models.News;
+import org.jug.montpellier.models.Speaker;
 import org.jug.montpellier.models.Yearpartner;
+import org.montpellierjug.store.jooq.Tables;
 import org.montpellierjug.store.jooq.tables.daos.YearpartnerDao;
 import org.wisdom.api.annotations.*;
 import org.wisdom.api.http.HttpMethod;
@@ -32,6 +40,8 @@ import org.wisdom.api.templates.Template;
 
 import java.beans.IntrospectionException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @Path("/admin/partner")
@@ -42,31 +52,51 @@ public class AdminPartnerController extends JugController {
 
     @Requires
     PropertySheet propertySheet;
+    @Requires
+    ListView listView;
 
     @Requires
     YearpartnerDao yearpartnerDao;
+    @Requires
+    DSLContext dslContext;
 
     public AdminPartnerController(@Requires JugSupport jugSupport) {
         super(jugSupport);
     }
 
     @Route(method = HttpMethod.GET, uri = "/")
-    public Result home() {
-        return template(template).render();
+    public Result all(@QueryParameter("search") String search) throws Exception {
+        SelectOrderByStep selectStep = dslContext.selectFrom(Tables.YEARPARTNER);
+        if (search != null && !search.isEmpty()) {
+            selectStep = ((SelectWhereStep)selectStep).where(org.montpellierjug.store.jooq.tables.Yearpartner.YEARPARTNER.NAME.likeIgnoreCase("%" + search + "%"));
+        }
+        List<Yearpartner> yearpartners = selectStep.orderBy(org.montpellierjug.store.jooq.tables.Yearpartner.YEARPARTNER.NAME.asc()).fetchInto(Yearpartner.class);
+        return template(template).withListview(listView.getRenderable(this, yearpartners, Yearpartner.class)).withParam("search", search).render();
     }
 
-
     @Route(method = HttpMethod.GET, uri = "/{id}")
-    public Result speaker(@Parameter("id") Long id) throws InvocationTargetException, ClassNotFoundException, IntrospectionException, IllegalAccessException {
+    public Result partner(@Parameter("id") Long id) throws InvocationTargetException, ClassNotFoundException, IntrospectionException, IllegalAccessException {
         Yearpartner yearpartner = Yearpartner.build(yearpartnerDao.findById(id));
         return template(template).withPropertySheet(propertySheet.getRenderable(this, yearpartner)).render();
-
     }
 
     @Route(method = HttpMethod.POST, uri = "/{id}")
-    public Result saveSpeaker(@Parameter("id") Long id, @Body Yearpartner yearpartner) throws InvocationTargetException, ClassNotFoundException, IntrospectionException, IllegalAccessException {
+    public Result savePartner(@Parameter("id") Long id, @Body Yearpartner yearpartner) throws InvocationTargetException, ClassNotFoundException, IntrospectionException, IllegalAccessException {
         yearpartnerDao.update(yearpartner.into(new org.montpellierjug.store.jooq.tables.pojos.Yearpartner()));
         return redirect("/admin/partner/" + id);
+    }
+
+    @Route(method = HttpMethod.GET, uri = "/new/")
+    public Result createPartner() throws ClassNotFoundException, IntrospectionException, IllegalAccessException, InvocationTargetException {
+        Map<String, Object> additionalParameters = Maps.newHashMap();
+        additionalParameters.put("cancelRedirect", "..");
+        return template(template).withPropertySheet(propertySheet.getRenderable(this, new Yearpartner(), additionalParameters)).render();
+    }
+
+    @Route(method = HttpMethod.POST, uri = "/new/")
+    public Result saveNewPartner(@Body Yearpartner yearpartner) {
+        yearpartnerDao.insert(yearpartner.into(new org.montpellierjug.store.jooq.tables.pojos.Yearpartner()));
+        return redirect("..");
     }
 
 }
