405fcfc85f547ad0af94feb1644f0527c090a03c
fteychene <francois.teychene@gmail.com>
2016-02-19 14:19:16 +0100

    Add new repository for wisdom-oauth. New version of structure adapted to Postgresql. Change Procfile and start processus to be adapted for dokku

--- start-dokku.sh
+++ start-dokku.sh
@@ -2,10 +2,9 @@
 
 # fix dokku
 export JAVA_HOME=/app/.jdk
-export PATH=$JAVA_HOME:$PATH
 #end fix dokku
 echo "Executing the running script"
 cd montpellier-jug-wisdom/target/wisdom/
 echo "Starting chameleon"
 export JVM_ARGS="-Dhttp.port=$PORT"
-./chameleon.sh start
+./chameleon.sh --interactive
