df512dd67beb6cd6bf79f2e7e648e93292d1013f
Olivier NOUGUIER <olivier.nouguier@hawk-platform.io>
2016-06-13 13:27:04 +0200

    Add paypal option for members.

--- montpellier-jug-wisdom/run.sh
+++ montpellier-jug-wisdom/run.sh
@@ -19,5 +19,10 @@ if [[ "$_java" ]]; then
        echo Java 8 needed :P read the manual.
        exit
     fi
-    mvn clean wisdom:run -DskipTests -DskipITs -Dtoken=$@
+    export DB_URL="jdbc:postgresql://localhost:5432/jug"
+    export DB_USER=jug
+    export DB_PASS=jug
+    export OAUTH_ID="360777772993-2ltneffc156df2mq5db9civvma98vels.apps.googleusercontent.com"
+    export OAUTH_SECRET="YsnPu9l7EfW66FIC87t4YziC"
+    mvn clean wisdom:run -DskipTests -DskipITs -Doauth.secret=YsnPu9l7EfW66FIC87t4YziC -Doauth.callback=http://localhost:9000/oauth2/cb -Doauth.authenticated=http://localhost:9000/admin/ -Dtoken=$@
 fi
